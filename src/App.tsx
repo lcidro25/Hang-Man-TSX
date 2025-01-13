3d59fd662b3447333e5bbaa59a7b3b57184fb689
Luis Joseph Cidro <luisjosephramoscidro@gmail.com>
2025-01-13 21:56:44 +0800

    First

---
+++ src/App.tsx
@@ -0,0 +1,116 @@
+import { useCallback, useEffect, useState } from "react";
+import mlbTeams from "./MLBTeams.json";
+import nbaTeams from "./NBATeams.json";
+import { HangmanDrawing } from "./HangmanDrawing.tsx";
+import { HangmanWord } from "./HangmanWord.tsx";
+import { Keyboard } from "./Keyboard.tsx";
+
+function getWord() {
+  const allTeams = [...mlbTeams, ...nbaTeams];
+  return allTeams[Math.floor(Math.random() * allTeams.length)];
+}
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(getWord);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const [hint, setHint] = useState("");
+
+  const incorrectLetters = guessedLetters.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
+
+  const isLoser = incorrectLetters.length >= 6;
+  const isWinner = wordToGuess
+    .split("")
+    .filter((letter) => letter !== " ")
+    .every((letter) => guessedLetters.includes(letter));
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return;
+      setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+    },
+    [guessedLetters, isWinner, isLoser]
+  );
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetters]);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (key !== "Enter") return;
+
+      e.preventDefault();
+      setGuessedLetters([]);
+      setWordToGuess(getWord());
+    };
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, []);
+
+  const showHint = () => {
+    if (mlbTeams.includes(wordToGuess)) {
+      setHint("MLB Team");
+    } else if (nbaTeams.includes(wordToGuess)) {
+      setHint("NBA Team");
+    }
+  };
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
+        {isWinner && "Winner! - Refresh to try guessing again."}
+        {isLoser && "You Lost! Nice Try. - Refresh to try guessing again."}
+      </div>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord
+        reveal={isLoser}
+        guessedLetters={guessedLetters}
+        wordToGuess={wordToGuess}
+      />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetters={guessedLetters.filter((letter) =>
+            wordToGuess.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
+      </div>
+      <button onClick={showHint}>Show Hint</button>
+      {hint && (
+        <div style={{ fontSize: "1.5rem", textAlign: "center" }}>{hint}</div>
+      )}
+    </div>
+  );
+}
+
+export default App;
